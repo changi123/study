667ffab14fcd4ffbae80feb067817c4963b1bb72
changi123 <133079671+changi123@users.noreply.github.com>
2025-01-02 17:25:55 +0900

    Feat : WordMapper 수정

--- src/main/java/hello/study/mapper/WordMapper.java
+++ src/main/java/hello/study/mapper/WordMapper.java
@@ -8,11 +8,25 @@ import hello.study.dto.QuestionDto;
 @Mapper
 public interface WordMapper {
 
-
+	// 단어 추가
 	void insertQuestion(QuestionDto questionDto);
 
-	void selectQuestion();
+	// 단어에 대한 예문 추가
+	void insertExample(ExampleDto exampleDto);
+
+	
+	// 추가된 단어 아이디 가져오기
+	int selectRecentQuestion();
+	
+	// 단어아이디 랜덤으로 뽑기
+	int randomQuestion();
+	
+	// 단어아이디에 따른 단어
+	QuestionDto selectByQuestionId(int question_id);
+
+	ExampleDto exampleDetail(int question_id);
+
+//	void selectQuestion();
 
 
-//	void insertExample(ExampleDto exampleDto);
 }
