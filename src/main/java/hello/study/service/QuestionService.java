680aa0560fa5e81ef40f5d3f7a3b467948c07b88
changi123 <133079671+changi123@users.noreply.github.com>
2025-01-02 17:26:07 +0900

    Feat : QuestionService 수정

--- src/main/java/hello/study/service/QuestionService.java
+++ src/main/java/hello/study/service/QuestionService.java
@@ -13,15 +13,42 @@ public class QuestionService {
 	@Autowired
 	private WordMapper wordMapper;
 
+	
+	// 단어 랜덤 아이디 조회
+	public int randomQuestion() {
+		int num = wordMapper.randomQuestion();
+		return num;
+		
+	}
+	
+	// 랜덤 아이디 단어 정보 조회 
+	public QuestionDto randomQuestionInfo(int question_id) {
+		return wordMapper.selectByQuestionId(question_id);
+		
+	}
+	
 	@Transactional
 	public void addQuestionWithExamples(QuestionDto question, ExampleDto exampleDto) {
-		// Question을 먼저 추가
-
-//		wordMapper.insertQuestion(question);
-		wordMapper.selectQuestion();
 		
-		// Question이 삽입된 후 자동으로 생성된 ID를 ExampleDto에 설정하여 추가
-		//	        exampleDto.setQuestionId(question.getId());
-		//	        wordMapper.insertExample(exampleDto);
+		wordMapper.insertQuestion(question);
+		
+		int questionNum = wordMapper.selectRecentQuestion();
+		
+		exampleDto.setQuestion_id(questionNum);
+		wordMapper.insertExample(exampleDto);
 	}
+
+	// 단어에 대한 예문 조회
+	public ExampleDto exampleDetail(int question_id) {
+		return wordMapper.exampleDetail(question_id);
+	}
+
+
+
+
+
+
+
+
+	
 }
